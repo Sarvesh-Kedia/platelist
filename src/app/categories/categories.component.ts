11708063822041b131c5a2d4f70faa405a3f3e97
Sarvesh-Kedia <sarvesh512kedia@gmail.com>
2022-12-16 22:49:49 +0530

    first commit

---
+++ src/app/categories/categories.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-categories',
+  templateUrl: './categories.component.html',
+  styleUrls: ['./categories.component.css']
+})
+export class CategoriesComponent {
+
+  categories = [
+    {
+        "id": "23",
+        "name": "Sick Days",
+        "items": ["khichdi", "upma"]
+    },
+    {
+        "id": "43",
+        "name": "Munchies",
+        "items": ["burger", "fries", "noodles"]
+    },
+    {
+        "id": "11",
+        "name": "Dont Feel Like Cooking",
+        "items": ["Shwarma", "Pasta"]
+    },
+    {
+        "id": "45",
+        "name": "Stress Eating",
+        "items": ["fries", "cheese balls"]
+    }
+
+  ]
+
+  
+  
+
+}
